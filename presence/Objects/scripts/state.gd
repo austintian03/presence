e1f39a9ecabd7503959fc6d21304c223f20704d5
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-08-28 18:59:07 -0400

    Create StateMachine and State base classes

---
+++ presence/Objects/scripts/state.gd
@@ -0,0 +1,19 @@
+class_name State
+extends Node
+
+signal transitioned
+
+var parent: CharacterBody2D
+var animations: AnimatedSprite2D
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func update(_delta: float):
+	pass
+
+func physics_update(_delta: float):
+	pass
