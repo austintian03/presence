edbbe2f15cf61f0f1c131e2b4593a5512d1580b9
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-08-28 23:48:57 -0400

    Add extra acceleration at peak jump height

--- presence/Player/scripts/states/jump.gd
+++ presence/Player/scripts/states/jump.gd
@@ -4,10 +4,14 @@ func enter():
 	parent.velocity.y = parent.JUMP_VELOCITY
 	animations.play("jump")
 
-func physics_update(_delta: float):
-	
+func physics_update(delta: float):
 	var movement = Input.get_axis("move_left", "move_right") * parent.SPEED
 	parent.velocity.x = movement
 	
-	if parent.is_on_floor() and parent.velocity.y > 0:
+	# add more velocity at peak of jump 
+	if parent.velocity.y > 0 and not parent.is_on_floor():
+		parent.velocity.y += 50*delta
+	
+	# transition back to floor
+	if parent.is_on_floor():
 		transitioned.emit(self, "idle")
