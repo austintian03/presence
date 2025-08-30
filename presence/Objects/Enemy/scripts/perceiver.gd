f7f24020feb3dc1383aec86097f570b40f230254
Austin Tian <47795058+austintian03@users.noreply.github.com>
2025-08-30 05:25:04 -0400

    Refactor and streamline Perceiver movement code

--- presence/Objects/Enemy/scripts/perceiver.gd
+++ presence/Objects/Enemy/scripts/perceiver.gd
@@ -7,6 +7,7 @@ signal player_detected
 @onready var animations: AnimatedSprite2D = $AnimatedSprite2d
 #@onready var state_machine: StateMachine = $StateMachine
 
+@export var do_flip: bool = false
 @export var patrol_radius: float = 90
 @export var speed: float = 30
 @export var movement_direction: Vector2 = Vector2.LEFT
@@ -18,8 +19,8 @@ var initial_pos: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animations.play("default")
-	apply_scale(Vector2(-1,1))
-	
+	#apply_scale(Vector2(-1,1))
+	velocity_vector = speed * movement_direction
 	initial_pos = position
 	front_pos = initial_pos + movement_direction * patrol_radius
 	f_sight_area.player_detected.connect(on_player_detected)
@@ -29,13 +30,12 @@ func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	velocity_vector = speed * movement_direction * delta
-	if position >= initial_pos:
-		velocity = velocity_vector
-		apply_scale(Vector2(-1,1))
-	elif position <= front_pos:
-		velocity = -velocity_vector
-		apply_scale(Vector2(-1,1))
+	if position == front_pos or position == initial_pos and velocity_vector.normalized() != movement_direction:
+		velocity_vector *= -1
+		if do_flip:
+			apply_scale(Vector2(-1,1))
+	
+	velocity = velocity_vector * delta
 	position += velocity
 
 func on_player_detected(is_perceiving: bool) -> void:
